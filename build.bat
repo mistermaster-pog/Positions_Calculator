ec3056d6c29879f914e03823a4c2181c0f99639b
bipel88 <karolnidecki776@gmail.com>
2025-01-03 22:12:27 +0000

    buil.bat creates /release folder

--- build.bat
+++ build.bat
@@ -1,4 +1,13 @@
 @echo off
 cd /d "%~dp0"
+:: Sprawdzanie, czy folder 'release' istnieje, jeśli nie to go tworzymy
+if not exist "release" (
+    mkdir "release"
+)
+
+:: Uruchomienie PyInstaller z odpowiednimi opcjami
 pyinstaller --onefile --icon="%~dp0images\icon.ico" --noconsole --distpath "%~dp0release\dist" --workpath "%~dp0release\build" --specpath "%~dp0release\spec" editor.py
+
+:: Powiadomienie o zakończeniu
+echo Export complete. The .exe file is in the release\dist folder.
 pause
